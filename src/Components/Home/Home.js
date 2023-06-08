e0c9ecc0794c0650511d6de23c99191e55b9265a
msaltzman5 <89426885+msaltzman5@users.noreply.github.com>
2023-06-07 19:02:00 -0500

    smooth scroll

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,24 +1,13 @@
-import React from 'react';
+import {debugging} from '../../App.js'
 
 import "./Home.css";
 //import { Button } from '@mui/material'
 
 function Home() {
 
-  function debugging() {
-    let p = []
-    for(let i = 0; i<1000; i++) {
-      p.push(<p>p {i+1}</p>)
-    }
-    
-    return p
-  }
-
   return (
-    <div className='home-content'>
-      <h1>
-        Home Page
-      </h1>
+    <div id='home' className='home-content'>
+      <h1>Home Page</h1>
       {debugging()}
     </div>
   );
