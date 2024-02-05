bd188f2e757e079203df281c318cce802ac4a137
msaltzman5 <89426885+msaltzman5@users.noreply.github.com>
2024-02-04 23:19:34 -0600

    Adding bear and buttons

--- src/Components/Valentine/Valentine.js
+++ src/Components/Valentine/Valentine.js
@@ -1,12 +1,19 @@
 import "./Valentine.css";
-// import Me from '../../Data/Me.jpg';
+// import Button from 'react-bootstrap/Button';
+import beargivingheart from '../../Data/bear-giving-heart.gif';
 
 
 
 function Valentine() {
   return (
     <div id='valentine' className='valentine-content'>
-      <h1>working</h1>
+      <div className="bear-giving-heart">
+        <img src={beargivingheart} alt="Sorry, I'm not loading" className="bear-giving-heart-gif"/>
+      </div>
+      <div className="valentine-buttons-container">
+        <button variant="outlined" className="no-button">No</button>
+        <button variant="outlined" className="yes-button">Yes</button>
+      </div>
     </div>
   );
 }
