0e5d551d7c006df7b9637c18495f9d4a7e77cb08
msaltzman5 <89426885+msaltzman5@users.noreply.github.com>
2024-08-04 00:08:06 -0500

    more image stuff :D

--- src/Components/ImageCarousel/ImageCarousel.js
+++ src/Components/ImageCarousel/ImageCarousel.js
@@ -4,7 +4,6 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import leftarrow from '../../Data/Icons/left-arrow-circle.png';
 import rightarrow from '../../Data/Icons/right-arrow-circle.png';
-import loading from '../../Data/Icons/loading.gif';
 
 
 const ImageCarousel = ({ images }) => {
@@ -29,21 +28,30 @@ const ImageCarousel = ({ images }) => {
     setImageLoading(false); // Set loading state to false when image has loaded
   };
 
+  const getFileName = (filePath) => {
+    return filePath.split('/').pop()
+      .replace(/\.[a-f0-9]{8,}\..*/, '')
+      .replace(/([a-z])([A-Z])/g, '$1 $2')
+      .replace(/([0-9])([A-Z])/g, '$1 $2')
+      .replace(/([a-zA-Z])([0-9])/g, '$1 $2');
+  };
+
   return (
-    <div className="image-carousel logo">
+    <div className="image-carousel">
       {/* TODO: add loading image for seamless transfer */}
-      {/* {imageLoading && (
-        <div className="loading-spinner">
-          <img src={loading} alt='Loading spinner not loading lol'></img>
-        </div>
-      )} */}
-      <img
-        src={images[currentIndex]}
-        alt={`Image ${currentIndex + 1}`}
-        onLoad={handleImageLoad}
-        style={{ display: imageLoading ? 'none' : 'block'}}
-        className="carousel-image"
-      />
+      {imageLoading && (
+        <p className='loading-text'>Loading...</p>
+      )}
+      <a href={images[currentIndex]} target="_blank" rel="noopener noreferrer">
+        <img
+          src={images[currentIndex]}
+          alt={`Image ${currentIndex + 1}`}
+          title={getFileName(images[currentIndex])}
+          onLoad={handleImageLoad}
+          style={{ display: imageLoading ? 'none' : 'block' }}
+          className="carousel-image"
+        />
+      </a>
       <div className="carousel-controls">
         <img src={leftarrow} alt='not loading' onClick={previousImage} />
         <img src={rightarrow} alt='not loading' onClick={nextImage} />
